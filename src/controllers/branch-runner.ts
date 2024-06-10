d91895096c4380dcdc59da59cd5219c5b35bd6e1
Sebastien Lafleur <sebastien.lafleur@gmail.com>
2024-06-10 18:12:46 -0400

    feature: mise en place d'un command line pour creer des branches
    mise en place d'un command line pour creer des branches
    
    [log]

--- src/controllers/branch-runner.ts
+++ src/controllers/branch-runner.ts
@@ -1,3 +1,21 @@
+import { spawnSync } from 'node:child_process';
+import { getDebugger } from '../helpers/pino.js';
+
 export class BranchRunner {
-  public async run(): Promise<void> {}
+  public async run(branch: string): Promise<void> {
+    // Definition de l'action
+    const command = 'git';
+
+    let parameters = ['fetch', '--all'];
+    getDebugger().info(`${command} ${parameters}`);
+    spawnSync(command, parameters);
+
+    parameters = ['pull'];
+    getDebugger().info(`${command} ${parameters}`);
+    spawnSync(command, parameters);
+
+    parameters = ['checkout', '-b', branch.toLowerCase()];
+    getDebugger().info(`${command} ${parameters}`);
+    spawnSync(command, parameters);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { readPackageSync } from 'read-pkg';
 import { program } from '@commander-js/extra-typings';
+import { BranchRunner } from './controllers/branch-runner.js';
 import { CommitRunner } from './controllers/commit-runner.js';
 import { handleError } from './helpers/handle-error.js';
 import { Logger } from './helpers/logger.js';
@@ -23,9 +24,12 @@ program.command('validate').action(async () => {
   await new CommitRunner().run();
 });
 
-program.command('branch').action(async () => {
-  await new CommitRunner().run();
-});
+program
+  .command('branch')
+  .argument('<branch>', 'branch to create')
+  .action(async (branch) => {
+    await new BranchRunner().run(branch);
+  });
 
 program.command('staging').action(async () => {
   await new CommitRunner().run();
